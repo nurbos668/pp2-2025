9cbe63f70eb4f91e2829fc058b85bd6b31075845
nurbos668 <nurbolsyn.tadzhikosov@gmail.com>
2025-01-30 13:49:26 +0600

    Initial commit

---
+++ week1/strings.py
@@ -0,0 +1,11 @@
+txt = "The best things in life are free!"
+print("free" in txt)
+
+
+txt = "The best things in life are free!"
+if "free" in txt:
+  print("Yes, 'free' is present.")
+
+
+b = "Hello, World!"
+print(b[-5:-2])
---
+++ week1/varibales.py
@@ -0,0 +1,25 @@
+x = 4       # x is of type int
+x = "Sally" # x is now of type str
+print(x)
+
+
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+
+
+num = 5
+str = "John"
+print(type(x))
+print(type(y))
+
+
+x = "awesome"
+
+def myfunc():
+  x = "fantastic"
+  print("Python is " + x)
+
+myfunc()
+
+print("Python is " + x)
